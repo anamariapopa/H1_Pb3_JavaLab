3b35279704b5bb12166b9c5ebbc97f2ea6b67642
anamariapopa <anamaria.2005p@gmail.com>
2025-10-20 00:30:40 +0300

    Second task done

--- src/BigNumberCalculator.java
+++ src/BigNumberCalculator.java
@@ -15,4 +15,23 @@ public class BigNumberCalculator {
         result[0] = carry;
         return result;
     }
+
+    //2
+    public static int[] substract(int[] a, int[] b) {
+        int n = a.length;
+        int[] result = new int[n];
+        int borrow = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int diff = a[i] - b[i] - borrow;
+            if (diff < 0) {
+                diff += 10;
+                borrow = 1;
+            }
+            else borrow = 0;
+            result[i] = diff;
+        }
+
+        return result;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,23 @@ public class Main {
     public static void main() {
         int[] a = {1, 3, 0, 0, 0, 0, 0, 0, 0};
         int[] b = {8, 7, 0, 0, 0, 0, 0, 0, 0};
+        int[] c = {8, 3, 0, 0, 0, 0, 0, 0, 0};
+        int[] d = {5, 4, 0, 0, 0, 0, 0, 0, 0};
 
         //1
-        int[] result = BigNumberCalculator.add(a, b);
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i]);
+        int[] resultAdd = BigNumberCalculator.add(a, b);
+        System.out.print("Addition = ");
+        for (int i = 0; i < resultAdd.length; i++) {
+            System.out.print(resultAdd[i]);
         }
 
         System.out.println();
 
         //2
+        int[] resultDiff = BigNumberCalculator.substract(c, d);
+        System.out.print("Substraction = ");
+        for (int i = 0; i < resultDiff.length; i++) {
+            System.out.print(resultDiff[i]);
+        }
     }
 }
